f54b79027e356afb781621c8f08a5ea7063f541a
Hien Le <lehien0893@gmail.com>
2026-02-27 12:17:58 +0700

    fix: layout about

--- app/layouts/about.vue
+++ app/layouts/about.vue
@@ -2,7 +2,7 @@
     <div class="text-white antialiased">
         <div class="fixed top-[-300px] left-1/2 -translate-x-1/2 w-[900px] h-[900px] bg-purple-600/10 blur-[200px] rounded-full -z-10"></div>
         <div class="max-w-6xl mx-auto px-6 lg:px-8 py-16 flex gap-16">
-            <LayoutAppAside></LayoutAppAside>
+            <slot />
             <LayoutAppBottomNav></LayoutAppBottomNav>
         </div>
     </div>
