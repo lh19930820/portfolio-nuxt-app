9d8423a1c46f1c67a6cb50c49ffe3947e200036c
Hien Le <lehien0893@gmail.com>
2026-02-27 12:28:00 +0700

    feat: review code

--- app/components/layout/AppAside.vue
+++ app/components/layout/AppAside.vue
@@ -32,6 +32,7 @@
                 </div>
 
             </div>
+            <!-- TODO: get value from .env -->
             <div class="flex gap-4 text-sm text-gray-500">
                 <a href="https://github.com/lh19930820" target="_blank" class="hover:text-white transition">GitHub</a>
                 <a href="https://www.linkedin.com/in/hien-le-6a90aa217/" class="hover:text-white transition">LinkedIn</a>
